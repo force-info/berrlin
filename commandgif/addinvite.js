2b253e8a55d1dd366a3d8e50d7c5214cc90a0e5a
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-31 18:16:02 +0300

    Update addinvite.js

--- commandgif/addinvite.js
+++ commandgif/addinvite.js
@@ -12,7 +12,7 @@ const embed = new Discord.MessageEmbed()
 .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
 .setAuthor(`Gif Invite`)
 .setImoge (`https://images-ext-1.discordapp.net/external/mYylpyqnCG0imci8zLHG7UE1Vropu-z4wv0S-adYedo/https/media.discordapp.net/attachments/848121416696070164/848210376364654602/image0.gif `)
-.setDescription(`**[Invite](https://discord.com/api/oauth2/authorize?client_id=772052634604404766&permissions=8&scope=bot) INVITE TO PORE BOT **`)
+.setDescription(`**[INVITES](https://discord.com/api/oauth2/authorize?client_id=772052634604404766&permissions=8&scope=bot) INVITE TO PORE BOT **`)
     
 message.channel.send({embed});
 }
