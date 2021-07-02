7f69f9698260fcefb9b5919b426e89c98d8ee74b
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-02 16:16:58 +0300

    Update invite.js

--- commandgif/invite.js
+++ commandgif/invite.js
@@ -10,7 +10,7 @@ const embed = new Discord.MessageEmbed()
 .setColor(`RANDOM`)
 .setThumbnail(message.author.avatarURL({dynamic: "true"}))
 .setTitle(`**invite**`)
-.setdescription(`[  click ](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot)`)
+.setDescription(`[  click ](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot)`)
 message.channel.send({embed});
 }
 
