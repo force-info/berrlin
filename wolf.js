18ab239a7154df67e36c8c4ac4d4b418e0f4e307
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-13 16:09:39 +0300

    Update wolf.js

--- wolf.js
+++ wolf.js
@@ -12,7 +12,11 @@ client.on('ready', async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
   
- client.user.setActivity(`>help |Teko bot `, { type:"LISTENING" })
+ client.user.setActivity(`>help | Verifay Bot Comming soon`, { type:"LISTENING" })
+
+client.user.setActivity(`Type: GuIdis ${client.guilds.cache.size},Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "LISTENING"});
+      }, (5000));
+
   console.log("${client.user.tag}")
 });
 
