539322baadf419ef653ee8fafb5c0ab909e7661c
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-14 11:29:48 +0300

    Update help.js

--- commandgif/help.js
+++ commandgif/help.js
@@ -24,8 +24,6 @@ const embed = new Discord.MessageEmbed()
 ** Photo Commands **
 \`pboy\` -\`pgirl
 
-**games Commands**
-\`howgay\`
 
 **Links**
 **[Invite](https://discord.com/api/oauth2/authorize?client_id=916776483366772756&permissions=8&scope=bot) - [Server Support](https://discord.gg/bYFjkzNTMg)**
