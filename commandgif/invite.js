48ed7cc9ee6fbeecf3215312fbeda9b61253255e
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-19 15:43:21 +0300

    Update invite.js

--- commandgif/invite.js
+++ commandgif/invite.js
@@ -9,8 +9,8 @@ const embed = new Discord.MessageEmbed()
     
 .setColor(`RANDOM`)
 .setThumbnail(message.author.avatarURL({dynamic: "true"}))
-.setTitle(`**invite**`)
-.setDescription(`[  click ](https://discord.com/api/oauth2/authorize?client_id=916776483366772756&permissions=8&scope=bot)`)
+.setTitle(`** > Oliver Bot **`)
+.setDescription(`Invite You Server [  click  ](https://discord.com/api/oauth2/authorize?client_id=916776483366772756&permissions=8&scope=bot)`)
 message.channel.send({embed});
 }
 
