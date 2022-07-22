7b6170d23ca2cc1871e6228560e1fd30aa068c35
Matheros <98669451+matheros0001@users.noreply.github.com>
2022-07-22 16:59:17 +0300

    Update invite.js

--- commandgif/invite.js
+++ commandgif/invite.js
@@ -1,17 +1,24 @@
 const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js")
+const { MessageButton } = require("discord-buttons")
 const client = new Discord.Client();
 const moment = require('moment');
 var config = require('../config.json');
 
 exports.run = async (client, message, args, msg) => {
 
-const embed = new Discord.MessageEmbed()
-    
-.setColor(`RANDOM`)
-.setThumbnail(message.author.avatarURL({dynamic: "true"}))
-.setTitle(`** > Oliver Bot **`)
-.setDescription(`Invite You Server [  click  ](https://discord.com/api/oauth2/authorize?client_id=916776483366772756&permissions=8&scope=bot)`)
-message.channel.send({embed});
+    let invite = new MessageButton().setStyle('url').setLabel('Invite Me').setURL('https://discord.com/api/oauth2/authorize?client_id=800442243697213442&permissions=8&scope=bot')
+    let support = new MessageButton().setStyle('url').setLabel('Support Server').setURL('https://discord.gg/bwfsd4qfkn')
+     
+      const allbuttons = [invite, support]
+
+‌   let mybuttonsmsg = await message.channel.send({    
+   .setColor(`RANDOM`)
+   .setThumbnail(message.author.avatarURL({dynamic: "true"}))
+   .setTitle(`** > Oliver Bot **`)
+   .setDescription(`Invite You Server [  click  ](https://discord.com/api/oauth2/authorize?client_id=916776483366772756&permissions=8&scope=bot)`)
+   buttons: allbuttons
+      });
 }
 
 exports.conf = {
